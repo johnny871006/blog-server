fd851a4195f5dcaf38ff6ac87b1d674dc634e28a
johnny <johnny871006@gmail.com>
2024-07-22 10:18:24 +0800

    框架設定 - 日誌處理

--- src/main/java/com/johnny/blogserver/web/IndexController.java
+++ src/main/java/com/johnny/blogserver/web/IndexController.java
@@ -2,18 +2,16 @@ package com.johnny.blogserver.web;
 
 import com.johnny.blogserver.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
-//        int i = 9/0;
-        String blog = null;
-        if(blog == null){
-            throw  new NotFoundException("此blog不存在");
-        }
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,String name){
+
+        System.out.println("------index--------");
         return "index";
     }
 }
