7d9ec10f588d9f63ae84b8f3006b68d5223310fb
johnny <johnny871006@gmail.com>
2024-07-22 14:10:39 +0800

    框架設定 - 頁面處理(重複部分使用fragment)

--- src/main/java/com/johnny/blogserver/web/IndexController.java
+++ src/main/java/com/johnny/blogserver/web/IndexController.java
@@ -1,17 +1,29 @@
 package com.johnny.blogserver.web;
 
 import com.johnny.blogserver.NotFoundException;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id,String name){
+    @GetMapping("/")
+    public String index(){
 
-        System.out.println("------index--------");
         return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+
+        return "about";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+
+        return "blog";
     }
 }
